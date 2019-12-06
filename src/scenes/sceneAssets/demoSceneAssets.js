9c5ca66deeb14d418d6edc245acbf90b947633d4
Darknov <darknov.dono@gmail.com>
2019-12-06 05:59:33 +0100

    Fix shots asset pack

--- src/scenes/sceneAssets/demoSceneAssets.js
+++ src/scenes/sceneAssets/demoSceneAssets.js
@@ -2,6 +2,7 @@ import { exhaust } from "./assetPacks/exhaustBlue";
 import { planets } from "./assetPacks/planets"
 import { ships } from "./assetPacks/ships"
 import { backgrounds } from "./assetPacks/backgrounds"
+import { shots } from "./assetPacks/shots";
 
 export const demoSceneAssets = [];
 
@@ -9,3 +10,4 @@ demoSceneAssets.push(...exhaust);
 demoSceneAssets.push(...planets);
 demoSceneAssets.push(...ships);
 demoSceneAssets.push(...backgrounds);
+demoSceneAssets.push(...shots);
